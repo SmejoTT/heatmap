c195ee56ce27036fbf1b8eb6ed0ab7671f24bdc6
SmejoTT <adrian.trajlinek@gmail.com>
2020-10-06 15:07:59 +0100

    refactoring, improved efficiency

--- heatmap/src/main/java/uk/ac/ed/inf/heatmap/App.java
+++ heatmap/src/main/java/uk/ac/ed/inf/heatmap/App.java
@@ -19,6 +19,11 @@ public class App {
     private static final int GRID_ROWS = 10;
     private static final int GRID_COLS = 10;
     
+    private static final BigDecimal EAST_BOUND = new BigDecimal("-3.184319");
+    private static final BigDecimal WEST_BOUND = new BigDecimal("-3.192473");
+    private static final BigDecimal NORTH_BOUND = new BigDecimal("55.946233");
+    private static final BigDecimal SOUTH_BOUND = new BigDecimal("55.942617");
+    
     private static final String GREEN = "#00ff00";
     private static final String MEDIUM_GREEN = "#40ff00";
     private static final String LIGHT_GREEN = "#80ff00";
@@ -31,21 +36,22 @@ public class App {
     
     
     private static Integer[][] predictionValues = new Integer[GRID_ROWS][GRID_COLS];
+    private static Point[][] gridPoints = new Point[GRID_ROWS+1][GRID_COLS+1];
     private static String heatmapJson; 
     
     
 
     private static void readFile(String fileName) throws FileNotFoundException {
-        File predictions = new File(fileName);
-        Scanner scanner = new Scanner(predictions);
+        var predictionsFile = new File(fileName);
+        var scanner = new Scanner(predictionsFile);
 
-        int iterator = 0;
+        var iterator = 0;
 
         while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] splitValues = line.split(",");
-            for (int i = 0; i < splitValues.length; i++) {
-                predictionValues[iterator][i]=Integer.parseInt(splitValues[i].trim()); 
+            var line = scanner.nextLine();
+            var valuesArray = line.split(",");
+            for (int i = 0; i < valuesArray.length; i++) {
+                predictionValues[iterator][i]=Integer.parseInt(valuesArray[i].strip()); 
             }
             iterator++;
         }
@@ -53,87 +59,72 @@ public class App {
     }
     
     private static String getPolygonColor(int col,int row) {
-        if ((predictionValues[row][col] >= 0) && (predictionValues[row][col] < 32) ) {
+        var predictionValue = predictionValues[row][col];
+        if ((predictionValue >= 0) && (predictionValue < 32) ) {
             return GREEN;
         }
         
-        if ((predictionValues[row][col] >= 32) && (predictionValues[row][col] < 64) ) {
+        if ((predictionValue >= 32) && (predictionValue < 64) ) {
             return MEDIUM_GREEN;
         }
         
-        if ((predictionValues[row][col] >= 64) && (predictionValues[row][col] < 96) ) {
+        if ((predictionValue >= 64) && (predictionValue < 96) ) {
             return LIGHT_GREEN;
         }
         
-        if ((predictionValues[row][col] >= 96) && (predictionValues[row][col] < 128) ) {
+        if ((predictionValue >= 96) && (predictionValue < 128) ) {
             return LIME_GREEN;
         }
         
-        if ((predictionValues[row][col] >= 128) && (predictionValues[row][col] < 160) ) {
+        if ((predictionValue >= 128) && (predictionValue < 160) ) {
             return GOLD;
         }
         
-        if ((predictionValues[row][col] >= 160) && (predictionValues[row][col] < 192) ) {
+        if ((predictionValue >= 160) && (predictionValue < 192) ) {
             return ORANGE;
         }
         
-        if ((predictionValues[row][col] >= 192) && (predictionValues[row][col] < 224) ) {
+        if ((predictionValue >= 192) && (predictionValue < 224) ) {
             return RED_ORANGE;
         }
         
-        if ((predictionValues[row][col] >= 224) && (predictionValues[row][col] < 256) ) {
+        if ((predictionValue >= 224) && (predictionValue < 256) ) {
             return RED;
         }
         
-        return "";
+        return null;
+    }
+    
+    private static void computeGridPoints() {
+        var longStep = (EAST_BOUND.subtract(WEST_BOUND)).divide(BigDecimal.TEN);
+        var latStep = (SOUTH_BOUND.subtract(NORTH_BOUND)).divide(BigDecimal.TEN);
+        for (int row = 0; row < GRID_ROWS+1; row++) {
+            var latitude = NORTH_BOUND.add(new BigDecimal(row).multiply(latStep)).doubleValue();
+            for (int col = 0; col < GRID_COLS+1; col++) {
+                var longitude = WEST_BOUND.add(new BigDecimal(col).multiply(longStep)).doubleValue();
+                gridPoints[row][col]= Point.fromLngLat(longitude, latitude);
+            }
+        }
     }
 
     private static void createHeatmapJson() {
-        BigDecimal eastBound = new BigDecimal("-3.184319");
-        BigDecimal westBound = new BigDecimal("-3.192473");
-        BigDecimal northBound = new BigDecimal("55.946233");
-        BigDecimal southBound = new BigDecimal("55.942617");
-
-        BigDecimal longStep = (eastBound.subtract(westBound)).divide(BigDecimal.TEN);
-        BigDecimal latStep = (southBound.subtract(northBound)).divide(BigDecimal.TEN);
-        System.out.println(longStep);
-        System.out.println(latStep);
-        
-        
-        List<Feature> listOfFeatures= new ArrayList<>();
+        var listOfFeatures= new ArrayList<Feature>();
         
         for (int row = 0; row < GRID_ROWS; row++) {
             for (int col = 0; col < GRID_COLS; col++) {
+                var topLeftPoint = gridPoints[row][col];
+                var topRightPoint = gridPoints[row][col+1];
+                var bottomRightPoint = gridPoints[row+1][col+1];
+                var bottomLeftPoint = gridPoints[row+1][col];
                 
-                List<Point> points = new ArrayList<>();
-                double longitude;
-                double latitude;
-                //Compute LONGITUDE LATITUDE for each POINT separately
+                var listOfPoints = new ArrayList<Point>();
+                listOfPoints.add(topLeftPoint);
+                listOfPoints.add(topRightPoint);
+                listOfPoints.add(bottomRightPoint);
+                listOfPoints.add(bottomLeftPoint);
+                var polygon = Polygon.fromLngLats(List.of(listOfPoints));
                 
-                longitude = westBound.add(new BigDecimal(col).multiply(longStep)).doubleValue();
-                latitude = northBound.add(new BigDecimal(row).multiply(latStep)).doubleValue();
-                Point topLeft = Point.fromLngLat(longitude, latitude);
-                
-                longitude = westBound.add(new BigDecimal(col+1).multiply(longStep)).doubleValue();
-                latitude = northBound.add(new BigDecimal(row).multiply(latStep)).doubleValue();
-                Point topRight = Point.fromLngLat(longitude, latitude);
-                
-                longitude = westBound.add(new BigDecimal(col+1).multiply(longStep)).doubleValue();
-                latitude = northBound.add(new BigDecimal(row+1).multiply(latStep)).doubleValue();
-                Point bottomRight = Point.fromLngLat(longitude, latitude);
-                
-                longitude = westBound.add(new BigDecimal(col).multiply(longStep)).doubleValue();
-                latitude = northBound.add(new BigDecimal(row+1).multiply(latStep)).doubleValue();
-                Point bottomLeft = Point.fromLngLat(longitude, latitude);
-                
-                points.add(topLeft);
-                points.add(topRight);
-                points.add(bottomRight);
-                points.add(bottomLeft);
-                
-                Polygon polygon = Polygon.fromLngLats(List.of(points));
-                
-                Feature feature = Feature.fromGeometry(polygon);
+                var feature = Feature.fromGeometry(polygon);
                 feature.addNumberProperty("opacity", 0.75);
                 feature.addStringProperty("rgb-string", getPolygonColor(col, row));
                 feature.addStringProperty("fill", getPolygonColor(col, row));
@@ -141,15 +132,15 @@ public class App {
                 listOfFeatures.add(feature);   
             }   
         }
-        FeatureCollection featureCollection = FeatureCollection.fromFeatures(listOfFeatures);
+        var featureCollection = FeatureCollection.fromFeatures(listOfFeatures);
         heatmapJson = featureCollection.toJson();
     }
     
     private static void outputHeatmapFile() {
         try {
-            File heatmap = new File("heatmap.geojson");
-            heatmap.createNewFile();
-            FileWriter writer = new FileWriter("heatmap.geojson");
+            var heatmapFile = new File("heatmap.geojson");
+            heatmapFile.createNewFile();
+            var writer = new FileWriter("heatmap.geojson");
             writer.write(heatmapJson);
             writer.close();
         } catch (IOException e) {
@@ -158,10 +149,9 @@ public class App {
         }    
     }
     
-    
-
     public static void main(String[] args) throws FileNotFoundException {
         readFile(args[0]);
+        computeGridPoints();
         createHeatmapJson();
         outputHeatmapFile();
     }
